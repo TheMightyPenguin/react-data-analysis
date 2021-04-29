6509a1eb0b1a905ce81dd2f65357fced9943c55d
Victor Tortolero <victor.tortolero@medwing.com>
2021-04-28 21:41:18 -0500

    Update LineChart props

--- src/components/LineChart/LineChart.tsx
+++ src/components/LineChart/LineChart.tsx
@@ -2,18 +2,36 @@ import React from "react";
 import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
 
-const data: Highcharts.Options = {
+export type Props = {
+  title: string;
+  subtitle: string;
+  yAxisTitle: string;
+  pointStart: number;
+  series: Highcharts.Options["series"];
+};
+
+const propsToOptions = ({
+  title,
+  subtitle,
+  yAxisTitle,
+  pointStart,
+  series,
+}: Props): Highcharts.Options => ({
+  chart: {
+    type: "line",
+  },
+
   title: {
-    text: "Solar Employment Growth by Sector, 2010-2016",
+    text: title,
   },
 
   subtitle: {
-    text: "Source: thesolarfoundation.com",
+    text: subtitle,
   },
 
   yAxis: {
     title: {
-      text: "Number of Employees",
+      text: yAxisTitle,
     },
   },
 
@@ -22,6 +40,8 @@ const data: Highcharts.Options = {
       rangeDescription: "Range: 2010 to 2017",
     },
   },
+
+  series,
 
   legend: {
     layout: "vertical",
@@ -34,32 +54,9 @@ const data: Highcharts.Options = {
       label: {
         connectorAllowed: false,
       },
-      pointStart: 2010,
+      pointStart,
     },
   },
-
-  series: [
-    {
-      name: "Installation",
-      data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175],
-    },
-    {
-      name: "Manufacturing",
-      data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434],
-    },
-    {
-      name: "Sales & Distribution",
-      data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387],
-    },
-    {
-      name: "Project Development",
-      data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227],
-    },
-    {
-      name: "Other",
-      data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111],
-    },
-  ],
 
   responsive: {
     rules: [
@@ -77,14 +74,12 @@ const data: Highcharts.Options = {
       },
     ],
   },
-};
+});
 
-export type Props = {
-  d?: false;
-};
-
-const LineChart: React.FC<Props> = () => {
-  return <HighchartsReact highcharts={Highcharts} options={data} />;
+const LineChart: React.FC<Props> = (props) => {
+  return (
+    <HighchartsReact highcharts={Highcharts} options={propsToOptions(props)} />
+  );
 };
 
 export default LineChart;
